c62b2256ed150b77d0b5e89d50b532bb6fc6cc83
Matthias Colin <matthias.colin@gmail.com>
2020-01-24 11:48:02 +0100

    First commit

---
+++ src/main/java/cinema/controller/MovieController.java
@@ -0,0 +1,123 @@
+package cinema.controller;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import cinema.persistence.entity.Movie;
+import cinema.persistence.repository.MovieRepository;
+import cinema.persistence.repository.PersonRepository;
+import cinema.service.IMovieService;
+
+@RestController
+@RequestMapping("/api/movie")
+public class MovieController {
+	
+	@Autowired
+	IMovieService movieService;
+	
+	@GetMapping
+	@ResponseBody
+	public List<Movie> allMovies() {
+		return movieService.getAllMovies();
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseBody
+	public Optional<Movie> movieById(@PathVariable("id") int idMovie) {
+		return movieService.getMovieById(idMovie);
+	}
+	
+	@GetMapping("/byTitle")
+	@ResponseBody
+	public Set<Movie> movieByPartialTitle(@RequestParam("t") String partialTitle) {
+		return movieService.getMovieByPartialTitle(partialTitle);
+	}
+	
+	@GetMapping("/byDirector")
+	@ResponseBody
+	public Set<Movie> findByDirector(@RequestParam("d") int idDirector){
+		return movieService.getMoviesByDirector(idDirector);
+	}
+	
+	@GetMapping("/byActor")
+	@ResponseBody
+	public Set<Movie> findByActor(@RequestParam("a") int idActor){
+		return movieService.getMoviesByActor(idActor);
+	}
+	
+//	@PostMapping
+//	@ResponseBody
+//	public Movie addMovie(@RequestBody Movie movie) {
+//		Movie movieSaved = movieRepository.save(movie);
+//		movieRepository.flush();
+//		return movieSaved;
+//	}
+//
+//	@PutMapping("/modify")
+//	@ResponseBody
+//	public Optional<Movie> modifyMovie(@RequestBody Movie movie) {
+//		// TODO : anywhere else
+//		var optMovie = movieRepository.findById(movie.getIdMovie());
+//		optMovie.ifPresent(m-> {
+//			m.setTitle(movie.getTitle());
+//			m.setYear(movie.getYear());
+//			m.setDuration(movie.getDuration());
+//			m.setDirector(movie.getDirector());
+//			movieRepository.flush();
+//		});
+//		//		
+//		return optMovie;
+//	}
+//	
+//	@PutMapping("/addActor")
+//	public Optional<Movie> addActor(@RequestParam("a") int idActor, @RequestParam("m") int idMovie) {
+//		// TODO : anywhere else
+//		var movieOpt = movieRepository.findById(idMovie);
+//		var actorOpt = personRepository.findById(idActor);
+//		if (movieOpt.isPresent() && actorOpt.isPresent()) {
+//			movieOpt.get().getActors().add(actorOpt.get());
+//			movieRepository.flush();
+//		}
+//		return movieOpt;
+//	}
+//	
+//	@PutMapping("/setDirector")
+//	public Optional<Movie> setDirector(@RequestParam("d") int idDirector, @RequestParam("m") int idMovie) {
+//		// TODO : anywhere else
+//		var movieOpt = movieRepository.findById(idMovie);
+//		var directorOpt = personRepository.findById(idDirector);
+//		if (movieOpt.isPresent() && directorOpt.isPresent()) {
+//			movieOpt.get().setDirector(directorOpt.get());
+//			movieRepository.flush();
+//		}
+//		return movieOpt;
+//	}
+//	
+//	
+//	@DeleteMapping("/{id}")
+//	@ResponseBody
+//	public Optional<Movie> deleteMovie(@PathVariable("id") int idMovie) {
+//		// TODO : anywhere else
+//		var movieToDelete = movieRepository.findById(idMovie);
+//		movieToDelete.ifPresent(m-> {
+//			movieRepository.delete(m);
+//			movieRepository.flush();
+//		});
+//		// 
+//		return movieToDelete;
+//	}
+}
---
+++ src/test/java/cinema/persistence/entity/test/TestMovieRepository.java
@@ -0,0 +1,180 @@
+package cinema.persistence.entity.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import cinema.persistence.entity.Movie;
+import cinema.persistence.entity.Person;
+import cinema.persistence.repository.MovieRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+class TestMovieRepository {
+
+	@Autowired
+	MovieRepository movieRepository;
+	
+	@Autowired
+	EntityManager entityManager;
+	
+	@Test
+	void testSave() {
+		Movie movie = new Movie("Joker", 2019);
+		movieRepository.save(movie);
+		var id = movie.getIdMovie();
+		System.out.println("Id new movie: " + id);
+		assertNotNull(id);
+	}
+	
+	
+	@Test
+	void testSaveWithDirector() {
+		// Given
+		Person person = new Person("Todd Phillips", LocalDate.of(1970, 12, 20));
+		Movie movie = new Movie("Joker", 2019, 165, person);
+		entityManager.persist(person); // already in cache
+		// when 
+		movieRepository.save(movie);
+		// then 
+		System.out.println(movie);
+		System.out.println(person);
+	}
+	
+	@Test
+	void testSelectAll() {
+		// given 
+		List<Movie> data = List.of(
+				new Movie("Joker", 2019),
+				new Movie("Parasite", 2019, 132),				
+				new Movie("Interstellar", 2014),				
+				new Movie("Gran Torino", 2008, 116)
+				);
+		data.forEach(entityManager::persist);
+		// when
+		var dataRead = movieRepository.findAll();
+		// then
+		dataRead.forEach(System.out::println);
+		assertEquals(data.size(), dataRead.size());
+		assertTrue(dataRead.stream()
+				.map(Movie::getTitle)
+				.allMatch(tr -> data.stream()
+							.map(Movie::getTitle)
+							.anyMatch(th -> th.equals(tr))
+						));
+	}
+	
+	@Test
+	void testFindById() {
+		// given
+		Movie movie = new Movie("Joker", 2019);
+		entityManager.persist(movie);
+		var id = movie.getIdMovie();
+		// when
+		var movieReadOpt = movieRepository.findById(id);
+		System.out.println(movieReadOpt);
+		assertAll(
+				()->assertTrue(movieReadOpt.isPresent()),
+				()->assertEquals(movie.getTitle(), movieReadOpt.get().getTitle()));
+		
+	}
+	
+	@Test
+	void testFindByTitle() {
+		// given 
+		String title = "Le Roi Lion";
+		List<Movie> data = List.of(
+				new Movie("Joker", 2019),
+				new Movie(title, 2019),				
+				new Movie(title, 1994)
+				);
+		data.forEach(entityManager::persist);
+		// when
+		var dataRead = movieRepository.findByTitle(title);
+		System.out.println(dataRead);
+	}
+	
+	@Test
+	void testFindByYearBetween() {
+		// given 
+		int year1 = 1995;
+		int year2 = 2015;
+		List<Movie> data = List.of(
+				new Movie("Joker", 2019),
+				new Movie("Le Roi Lion", 1994),				
+				new Movie("Seven", year1),
+				new Movie("Mad Max: Fury Road", year2),
+				new Movie("Gran Torino", 2008)
+				);
+		data.forEach(entityManager::persist);
+		// when
+		var dataRead = movieRepository.findByYearBetween(year1, year2);
+		// then
+		System.out.println(dataRead);
+		assertAll(
+				() -> assertEquals(3, dataRead.size()),
+				() -> assertTrue(dataRead.stream()
+						.mapToInt(Movie::getYear)
+						.allMatch(y -> (y >= year1) && (y <= year2))));
+		
+	}
+	
+	@Test
+	void testFindByTitleAndYear() {
+		// given 
+		String title = "Le Roi Lion";
+		int year = 1994;
+		List<Movie> data = List.of(
+				new Movie("Forest Gump", year),
+				new Movie(title, year),				
+				new Movie(title, 2019));
+		data.forEach(entityManager::persist);
+		// when
+		var dataRead = movieRepository.findByTitleAndYear(title, year);
+		// then
+		System.out.println(dataRead);
+		// TODO : asserts
+	}	
+	
+	@Test
+	void testFindByActorsNameEndingWith() {
+		// given
+		var roiLion = new Movie("Le Roi Lion", 1994);  				
+		var armeFatale = new Movie("L'Arme Fatale", 1987); 
+		var madMax = new Movie("Mad Max", 1978);
+		var movies = List.of(roiLion, armeFatale, madMax);
+		movies.forEach(entityManager::persist);
+		var melGibson = new Person("Mel Gibson");
+		var whoopi = new Person("Whoopi Goldberg");
+		var danny = new Person("Danny Glover");
+		entityManager.persist(melGibson);
+		entityManager.persist(whoopi);
+		entityManager.persist(danny);
+		roiLion.getActors().add(whoopi);
+		madMax.getActors().add(melGibson);
+		Collections.addAll(armeFatale.getActors(), melGibson, danny);
+		entityManager.flush();
+		// when
+		var moviesWithMel = movieRepository.findByActorsNameEndingWith("Gibson");
+		// then 
+		assertAll(
+				()->assertTrue(moviesWithMel.contains(madMax), "Mad Max"),
+				()->assertTrue(moviesWithMel.contains(armeFatale), "Arme Fatale"),
+				()->assertFalse(moviesWithMel.contains(roiLion), "Roi Lion"));
+	}
+}
